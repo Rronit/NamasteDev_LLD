e46e474c159a8af190ab18214b6878754166cb83
Rronit <rronit2015@gmail.com>
2024-10-03 07:25:43 +0530

    feat: infinite scroll

--- src/components/ShimmerUI/ShimmerUI.tsx
+++ src/components/ShimmerUI/ShimmerUI.tsx
@@ -3,27 +3,38 @@ import { MemeCard } from "./memeCard";
 import DummyCard from "./dummyCard";
 export const ShimmerUI = () => {
   const [memes, setMemes] = useState<any[]>([]);
+  const [showShimmer, setShowShimmer] = useState<boolean>(false);
   async function fetchMemes() {
+    setShowShimmer(true);
     const resp = await fetch("https://meme-api.com/gimme/20");
     const data = await resp.json();
-    console.log(data.memes);
-    setMemes(data.memes);
+    setMemes(() => [...memes, ...data.memes]);
+    setShowShimmer(false);
+  }
+  function handleScroll() {
+    if (window.scrollY + window.innerHeight === document.body.scrollHeight) {
+      console.log(
+        window.scrollY + window.innerHeight + 5,
+        document.body.scrollHeight
+      );
+      !showShimmer && fetchMemes();
+    }
   }
   useEffect(() => {
     fetchMemes();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {memes.length > 0 ? (
-          memes.map((item: any, idx: number) => {
-            console.log(typeof item, item, idx);
-            return <MemeCard key={item.url} memeData={item} />;
-          })
-        ) : (
-          <DummyCard />
-        )}
+        {memes.map((item: any) => {
+          return <MemeCard key={item.url} memeData={item} />;
+        })}
+        {showShimmer && <DummyCard />}
       </div>
     </>
   );
